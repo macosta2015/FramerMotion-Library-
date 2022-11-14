cc58c5aca688c23ecb550565852a451585ce91d9
macosta2015 <macosta2015@my.fit.edu>
2022-11-13 22:43:26 -0500

    Using different properties.

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import {motion} from 'framer-motion';
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring', 
+      mass: 0.4,
+      damping: 8,
+      // delay:0.5,
+      when: "beforeChildren", // This firsts needs to be animated and then the children go,
+      staggerChildren: 0.4
+    }
+  }
+}
+
+const childVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    }
+}
 
 const Order = ({ pizza }) => {
   return (
-    <div className="container order">
+    <motion.div className="container order"
+      variants={containerVariants}
+      initial="hidden" //Initial animation
+      animate="visible" //After we arrive to the animation
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
+      {/* We do not need to declare them becuase they are being propragrated from the parent. 
+      They have the same parent.  */}
+      <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
+      <motion.div variants={childVariants}>
+        {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+      </motion.div>
+    </motion.div>
   )
 }
 
