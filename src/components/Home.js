e097658bb24bb487de59902363ce03cd4c7ee3e4
macosta2015 <macosta2015@my.fit.edu>
2022-11-16 16:41:57 -0500

    Creating a loader by using Framer Motion

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import Loader from './Loader';
 
 const buttonVariants = {
   hover: {
@@ -10,7 +11,8 @@ const buttonVariants = {
     transition: {
       // yoyo: 10 //10 repetitions
       duration: 0.5,
-      yoyo: Infinity
+      yoyo: Infinity,
+      ease: 'easeOut'
     }
   }
 }
@@ -46,6 +48,7 @@ const Home = () => {
           Create Your Pizza
         </motion.button>
       </Link>
+      <Loader />
     </motion.div>
   )
 }
